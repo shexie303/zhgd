96491278c034dd73612b62cae0f2500b78e99319
Jerry <ajie20@163.com>
2018-08-25 17:09:49 +0800

    二级 表格

--- resources/views/default/electric/index.blade.php
+++ resources/views/default/electric/index.blade.php
@@ -1,13 +1,243 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>电力控制</title>
-    </head>
-    <body>
-        @include('default/common/header')
-        <p><a href="/auth/logout">退出</a></p>
-    </body>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>电力控制</title>
+	<link rel="stylesheet" href="{{ URL::asset('src/static/css/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('src/static/css/common.css') }}">
+</head>
+<body>
+	<div id="page" class="main-cate-bg">
+		<div class="main-header--box">
+			<div class="main-header">
+				<div class="sn-home-link"></div>
+				<div class="logo">宝坻欣鼎智慧工地物联网综合管理平台（金玉六园）</div>
+				<ul class="sn-quick-menu">
+					<li class="sn-bell"></li>
+					<li class="sn-profile">
+						<div class="sn-menu">
+							<div class="menu-hd"></div>
+							<div class="menu-bd">
+								<div class="menu-bd-panel">
+									<a href="javascript:void(0);">syh156254</a>
+									<a href="/auth/logout">退出</a>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="main-grid-table">
+			<div class="monitor-con">
+				<div class="monitor-inner-con">
+					<div class="monitor-border">
+						<table class="monitor-info-table">
+							<thead>
+								<tr>
+									<th width="300">
+										<div class="clip-path">
+											<b>类别</b>
+											<em>名称</em>
+										</div>
+									</th>
+									<th>电压</th>
+									<th>电流</th>
+									<th>有功功率</th>
+									<th>无功功率</th>
+									<th>电?</th>
+									<th>实时功率</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+								<tr>
+									<td>第2号站第4号闸</td>
+									<td>200V</td>
+									<td>50A</td>
+									<td>300W</td>
+									<td>200Var</td>
+									<td>200V</td>
+									<td>200W</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
 </html>
--- resources/views/default/userinfo/index.blade.php
+++ resources/views/default/userinfo/index.blade.php
@@ -1,13 +1,179 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>人员信息</title>
-    </head>
-    <body>
-        @include('default/common/header')
-        <p><a href="/auth/logout">退出</a></p>
-    </body>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>人员信息</title>
+	<link rel="stylesheet" href="{{ URL::asset('src/static/css/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('src/static/css/common.css') }}">
+</head>
+<body>
+	<div id="page" class="main-cate-bg">
+		<div class="main-header--box">
+																	 <div class="main-header">
+																	 <div class="sn-home-link"></div>
+																	 <div class="logo">宝坻欣鼎智慧工地物联网综合管理平台（金玉六园）</div>
+																	 <ul class="sn-quick-menu">
+																	 <li class="sn-bell"></li>
+																	 <li class="sn-profile">
+																	 <div class="sn-menu">
+																	 <div class="menu-hd"></div>
+																	 <div class="menu-bd">
+																	 <div class="menu-bd-panel">
+																	 <a href="javascript:void(0);">syh156254</a>
+																	 <a href="/auth/logout">退出</a>
+																	 </div>
+																	 </div>
+																	 </div>
+																	 </li>
+																	 </ul>
+																	 </div>
+																	 </div>
+		<div class="main-grid-table">
+			<div class="monitor-con">
+				<div class="monitor-inner-con">
+					<div class="monitor-border">
+						<table class="monitor-info-table">
+							<thead>
+								<tr>
+									<th>姓名</th>
+									<th>工号</th>
+									<th>工种</th>
+									<th>所属</th>
+									<th>性别</th>
+									<th>籍贯</th>
+									<th>入场时间</th>
+									<th>离场时间</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+								<tr>
+									<td>解放军将</td>
+									<td>0068</td>
+									<td>安全员</td>
+									<td>电力一班</td>
+									<td>男</td>
+									<td>乌鲁木齐</td>
+									<td>2018-06-26 12:24:12</td>
+									<td>2018-06-26 18:12:45</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
 </html>
