353b25b2645d7ea18038889ef9667d6817d2e099
chenzhenhua007 <chenzhenhua_007@163.com>
2018-08-26 11:40:58 +0800

    优化，事件订阅中心的数据结构。
    1、ext_info字段改为serialize序列化。
    2、添加event_log_id，用于后续操作中，利用log_id进行更新的操作

--- activemq_event/event_accept.php
+++ activemq_event/event_accept.php
@@ -1,19 +1,25 @@
 <?php
 include 'vendor/autoload.php';
 include '../Db.php';
+
 include 'Event/CommEventLog.php';
+include 'Event/TriggerResult.php';
+include 'Event/CommEventTrig.php';
+include 'Event/EventState.php';
+include 'Event/MsgCmdType.php';
+
 include 'GPBMetadata/EventDis.php';
 include 'GPBMetadata/Comm.php';
 
 try {
   $url = 'tcp://120.27.31.232:6008'; //地址
   $stomp = new Stomp($url, 'admin', '1234567');
-  $stomp->subscribe('/topic/openapi.vss.topic'); //消息队列
-  
+  $stomp->subscribe('/topic/openapi.vss.topic'); //视频监控-消息队列
+  //$stomp->subscribe('/topic/openapi.ias.topic'); //入侵报警-消息队列
   //$start = now();
   $count = 0;
   echo "Waiting for messages...\n";
-  while(true) {
+  while (true) {
       $hasFrame = $stomp->hasFrame();
       //var_dump($hasFrame);
       if ($hasFrame) {
@@ -24,6 +30,7 @@ try {
                   //解析事件内容
                   decode_pd_msg($frame->body);
               } else {
+                  var_dump($frame);
                   echo "Unexpected frame.\n";
               }
           }
@@ -65,9 +72,23 @@ function decode_pd_msg($body) {
         foreach ($log as $key => $value) {
             $log[$key] = !empty($value) ? $value : '';
         }
-
-        //保存
-        save_to_database($log);
+        
+        //说明是联动信息，进入区域，热成像：暂时不必和表中已有记录整合，因为联动提供的图片地址在XML中已包含。
+        //此处，严格来写，应为利用log_id验证数据表中是否已有记录。但这2项验证，也能验证是否为联动
+        if (empty($log['event_type']) && is_object($log['rslt_msg'])) {
+            //var_dump($log['rslt_msg']->getTriggerInfo());
+//             $db = new \DB();
+//             $where = "event_log_id = '{$log['log_id']}'";
+//             $res = $db->table('site_error_report')->where($where)->limit(1)->select('id, event_log_id, ext_info');
+//             if ($res) {
+//                 if ($res[0][2]) { //ext_info
+//                     //...
+//                 }
+//             }
+        } else {
+            //保存
+            save_to_database($log);
+        }        
     } else {
         //其他处理操作...
     }
@@ -79,7 +100,8 @@ function save_to_database($data = null) {
         $insertData['event_name'] = $data['event_type_name'].':'.$data['event_name'];
         $insertData['event_type'] = 'video'; //视频事件
         $insertData['event_state'] = 1; //默认未处理
-        $insertData['ext_info'] = json_encode($data); //序列化
+        $insertData['event_log_id'] = $data['log_id'];
+        $insertData['ext_info'] = serialize($data); //序列化
         $insertData['created_at'] = date('Y-m-d H:i:s');
         $insertData['updated_at'] = date('Y-m-d H:i:s');
         
