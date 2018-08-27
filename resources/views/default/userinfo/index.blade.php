c2ab0490a3fd286fd039a382fe822a65efd3e891
Jerry <ajie20@163.com>
2018-08-27 13:16:30 +0800

    人员定位

--- resources/views/default/userinfo/index.blade.php
+++ resources/views/default/userinfo/index.blade.php
@@ -1,13 +1,25 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>门禁管控</title>
-    </head>
-    <body>
-        @include('default/common/header')
-        <p><a href="/auth/logout">退出</a></p>
-    </body>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>天津市津房置业有限公司宝坻项目群管理平台 -- 人员定位</title>
+	<link rel="stylesheet" href="{{ URL::asset('src/static/css/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('src/static/css/common.css') }}">
+</head>
+<body>
+	<div id="page" class="main-cate-bg">
+		@include('default/common/header')
+		<div class="main-grid-table">
+			<div class="monitor-con">
+				<div class="monitor-inner-con">
+					<div class="monitor-border">
+						<img src="http://120.27.31.232:6036/hg/uploadImages/1535249268296.png" alt="" width="1579">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
 </html>
