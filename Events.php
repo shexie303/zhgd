17acf3a264aa4b8d57eae6ed3a36db5499908bc6
luchao <luchao@admin.com>
2018-09-01 14:35:14 +0800

    模块修改

--- Events.php
+++ Events.php
@@ -170,7 +170,8 @@ class Events
 		           $eventName = array(
 		               'video'     => '视频监控',
 		               'electric'  => '电力监控',
-		               'tower'     => '塔吊&升降机',
+		               'tower'     => '塔吊',
+		               'elevator'  => '升降机',
 		               'door'      => '门禁',
 		               'userinfo'  => '人员定位'
 		           );
