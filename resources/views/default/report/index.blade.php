9493bcf8bf962d0786dcfb57277963ae2e29d787
Jerry <ajie20@163.com>
2018-09-11 23:16:51 +0800

    消息中心

--- resources/views/default/report/index.blade.php
+++ resources/views/default/report/index.blade.php
@@ -31,6 +31,7 @@
 			transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
 			position: relative;
 		}
+
 		.ant-btn-handle {
 			-webkit-box-shadow: 0 0 1px rgba(41, 223, 233, 0.75), 0 1px 3px rgba(2, 46, 49, .75), 0 0 6px rgba(5, 192, 202, .75) inset;
 			box-shadow: 0 0 1px rgba(41, 223, 233, 0.75), 0 1px 3px rgba(2, 46, 49, .75), 0 0 6px rgba(5, 192, 202, .75) inset;
@@ -43,11 +44,13 @@
 			/*-webkit-box-shadow: 0 2px 0 rgba(0, 0, 0, 0.035);*/
 			/*box-shadow: 0 2px 0 rgba(0, 0, 0, 0.035);*/
 		}
-		.ant-btn-handle:hover{
+
+		.ant-btn-handle:hover {
 			background: #166975;
 			color: #fff;
 		}
-		.ant-btn[disabled]{
+
+		.ant-btn[disabled] {
 			cursor: not-allowed;
 			color: #76999f;
 			background: linear-gradient(#3a4b4d, #182326);
@@ -57,117 +60,110 @@
 	</style>
 </head>
 <body>
-	<div id="page" class="main-cate-bg">
-		@include('default/common/header')
-		<div class="monitor-con">
-			<div class="monitor-inner-con">
-				<div class="monitor-border">
-					<table class="monitor-info-table">
-						<thead>
+<div id="page" class="main-cate-bg">
+	@include('default/common/header')
+	<div class="monitor-con">
+		<div class="monitor-inner-con">
+			<div class="monitor-border">
+				<table class="monitor-info-table">
+					<thead>
+					<tr>
+						<th>类别</th>
+						<th>名称</th>
+						<th>报警内容</th>
+						<th>报警时间</th>
+						<th>状态</th>
+						<th>操作</th>
+					</tr>
+					</thead>
+					<tbody>
+					@foreach ($list as $value)
 						<tr>
-							<th>类别</th>
-							<th>名称</th>
-							<th>报警内容</th>
-							<th>报警时间</th>
-							<th>状态</th>
-							<th>操作</th>
+							<td>报警信息</td>
+							<td>{{$value['event_name']}}</td>
+							<td>{{$value['event_msg']}}</td>
+							<td>{{$value['created_at']}}</td>
+							@if ($value['event_state'] == '1')
+								<td>未处理</td>
+								<td>
+									<button type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
+									<button type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
+								</td>
+							@elseif ($value['event_state'] == '2')
+								<td>处理中</td>
+								<td>
+									<button type="button" class="ant-btn ant-btn-handle"><span>完成</span></button>
+									<button disabled type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
+								</td>
+							@elseif ($value['event_state'] == '3')
+								<td>已完成</td>
+								<td>
+									<button disabled type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
+									<button disabled type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
+								</td>
+							@elseif ($value['event_state'] == '4')
+								<td>已忽略</td>
+								<td>
+									<button disabled type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
+									<button disabled type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
+								</td>
+							@else
+								<td>未定义</td>
+								<td>
+									<button type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
+									<button type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
+								</td>
+							@endif
 						</tr>
-						</thead>
-						<tbody>
-						    @foreach ($list as $value)
-							<tr>
-								<td>报警信息</td>
-								<td>{{$value['event_name']}}</td>
-								<td>{{$value['event_msg']}}</td>
-								<td>{{$value['created_at']}}</td>
-								@if ($value['event_state'] == '1')
-								    <td>未处理</td>
-								    <td>
-								        <button type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
-									    <button type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
-								    </td>
-								@elseif ($value['event_state'] == '2')
-								    <td>处理中</td>
-								    <td>
-								        <button type="button" class="ant-btn ant-btn-handle"><span>完成</span></button>
-									    <button disabled type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
-								    </td>
-								@elseif ($value['event_state'] == '3')
-								    <td>已完成</td>
-								    <td>
-								        <button disabled type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
-									    <button disabled type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
-								    </td>
-								@elseif ($value['event_state'] == '4')
-								    <td>已忽略</td>
-								    <td>
-								        <button disabled type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
-									    <button disabled type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
-								    </td>
-								@else
-								    <td>未定义</td>
-								    <td>
-								        <button type="button" class="ant-btn ant-btn-handle"><span>处理</span></button>
-									    <button type="button" class="ant-btn ant-btn-ignore"><span>忽略</span></button>
-								    </td>
-								@endif
-							</tr>
-						    @endforeach
-						</tbody>
-					</table>
-				</div>
+					@endforeach
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
-	<div id="sendMessageModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="sendMessageModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h4 class="modal-title" id="myModalLabel">请选择发送短信组（可多选）</h4>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
+</div>
+<div id="sendMessageModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="sendMessageModalLabel"
+     aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="myModalLabel">请选择发送短信组（可多选）</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<input type="hidden" id="reportId">
+				<div class="custom-control custom-checkbox">
+					<input type="checkbox" class="custom-control-input" id="customCheck1">
+					<label class="custom-control-label" for="customCheck1">消防安全组</label>
 				</div>
-				<div class="modal-body">
-					<input type="hidden" id="reportId">
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" id="customCheck1">
-						<label class="custom-control-label" for="customCheck1">消防安全组</label>
-					</div>
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" id="customCheck2">
-						<label class="custom-control-label" for="customCheck2">消防安全组</label>
-					</div>
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" id="customCheck3">
-						<label class="custom-control-label" for="customCheck3">消防安全组</label>
-					</div>
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" id="customCheck4">
-						<label class="custom-control-label" for="customCheck4">消防安全组</label>
-					</div>
+				<div class="custom-control custom-checkbox">
+					<input type="checkbox" class="custom-control-input" id="customCheck2">
+					<label class="custom-control-label" for="customCheck2">消防安全组</label>
 				</div>
-				<div class="modal-footer">
-					<button type="button" id="J_SendMessage" class="btn btn-primary">
-						发送短信
-					</button>
+				<div class="custom-control custom-checkbox">
+					<input type="checkbox" class="custom-control-input" id="customCheck3">
+					<label class="custom-control-label" for="customCheck3">消防安全组</label>
 				</div>
+				<div class="custom-control custom-checkbox">
+					<input type="checkbox" class="custom-control-input" id="customCheck4">
+					<label class="custom-control-label" for="customCheck4">消防安全组</label>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" id="J_SendMessage" class="btn btn-primary">
+					发送短信
+				</button>
 			</div>
 		</div>
 	</div>
+</div>
 <script src="{{ URL::asset('src/static/js/jquery.js') }}"></script>
 <script src="{{ URL::asset('src/static/js/bootstrap.js') }}"></script>
 <script>
 	$(function () {
-		// function getMessageList() {
-		// 	$.ajax({
-		// 		type: "POST",
-		// 		dataType: "json",
-		// 		async: false,
-		// 		url: 'http://118.190.137.205'
-		// 	})
-		// }
-		$('#sendMessageModal').on('show.bs.modal', function(e) {
+		$('#sendMessageModal').on('show.bs.modal', function (e) {
 			var button = $(e.relatedTarget);
 			var recipient = button.data('whatever');
 			var modal = $(this);
