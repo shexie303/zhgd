15034762400253b3d57bc5dbd0fbf7066852d43c
luchao <luchao@admin.com>
2018-09-15 16:16:48 +0800

    升降机二级页面

--- app/Http/Controllers/Home/ElevatorController.php
+++ app/Http/Controllers/Home/ElevatorController.php
@@ -13,7 +13,39 @@ class ElevatorController extends HomeController
         $id = (int) $request->input('id');
         if($type == 2){
             //升降机
-            return view(self::HOME_VIEW_PREFIX . '/elevator/elevator');
+            $number_arr = [];
+            $devices = SiteDevices::where(['type' => 'elevator', 'construction_id' => session('login_user_constructions_id')])->get();
+            foreach($devices as $key => $val){
+                $number_arr[$val->id] = $val->number;
+                if($id){
+                    if($id == $val->id){
+                        $val->current = 1;
+                    }else{
+                        $val->current = 0;
+                    }
+                }else{
+                    if($key > 0){
+                        $val->current = 0;
+                    }else{
+                        $val->current = 1;
+                    }
+                }
+                $val->url = '/elevator?type=2&id='.$val->id;
+            }
+            if(!$id){
+                $id = $devices[0]->id;
+            }
+            $map = ['number' => $number_arr[$id], 'type' =>$type];
+            $data = SiteElevatorLogs::where($map)->orderBy('id', 'desc')->first();
+            if(!$data){
+                $data = new \stdClass();
+                $data->weight = 0;
+                $data->height = 0;
+                $data->wind_speed = 0;
+                $data->speed = 0;
+                $data->online = 2;
+            }
+            return view(self::HOME_VIEW_PREFIX . '/elevator/elevator', ['ws' => $map, 'devices' => $devices, 'data' => $data]);
         }else{
             //塔吊
             $number_arr = [];
@@ -39,7 +71,7 @@ class ElevatorController extends HomeController
                 $id = $devices[0]->id;
             }
             $map = ['number' => $number_arr[$id], 'type' =>$type];
-            $data = SiteElevatorLogs::where($map)->first();
+            $data = SiteElevatorLogs::where($map)->orderBy('id', 'desc')->first();
             if(!$data){
                 $data = new \stdClass();
                 $data->weight = 0;
