d364a7e25da5be56b7b7ce122f706662f3eb8a72
Jerry <ajie20@163.com>
2018-11-07 00:01:07 +0800

    人员定位 嵌套页面

--- resources/views/default/userinfo/index.blade.php
+++ resources/views/default/userinfo/index.blade.php
@@ -11,32 +11,41 @@
 <body>
 	<div id="page" class="main-cate-bg">
 		@include('default/common/header')
+		{{--<div class="main-grid-table">--}}
+			{{--<div class="monitor-con-squar">--}}
+				{{--<div class="monitor-inner-con">--}}
+					{{--<div class="monitor-border">--}}
+						{{--<img src="{{ URL::asset('src/static/img/info_monitor.png') }}" alt="">--}}
+						{{--<iframe src="http://120.27.31.232:6036/hg/getMonitor.do?username=data&password=123456" width="1280" height="800"></iframe>--}}
+						{{--<span class="info-dot dot-1"></span>--}}
+						{{--<span class="info-dot dot-2"></span>--}}
+					{{--</div>--}}
+				{{--</div>--}}
+			{{--</div>--}}
+		{{--</div>--}}
 		<div class="main-grid-table">
 			<div class="monitor-con-squar">
 				<div class="monitor-inner-con">
 					<div class="monitor-border">
-						<img src="{{ URL::asset('src/static/img/info_monitor.png') }}" alt="">
-						{{--<iframe src="http://120.27.31.232:6036/hg/getMonitor.do?username=data&password=123456" width="1280" height="800"></iframe>--}}
-						<span class="info-dot dot-1"></span>
-						<span class="info-dot dot-2"></span>
+						<iframe src="http://120.27.31.232:6036/hg/getMonitor.do?username=data&password=123456" width="1280" height="800"></iframe>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<script src="{{ URL::asset('src/static/js/jquery.js') }}"></script>
-	<script>
-		$(function () {
-			setInterval(function () {
-				var randomX = Math.ceil(Math.random() * 1280) - 32;
-				var randomY = Math.ceil(Math.random() * 800) - 41;
-				var random = Math.ceil(Math.random() * 2);
-				$('.monitor-border').find('.dot-' + random).css({
-					left: randomX,
-					top: randomY
-				})
-			}, 1000 * 30)
-		})
-	</script>
+	{{--<script src="{{ URL::asset('src/static/js/jquery.js') }}"></script>--}}
+	{{--<script>--}}
+		{{--$(function () {--}}
+			{{--setInterval(function () {--}}
+				{{--var randomX = Math.ceil(Math.random() * 1280) - 32;--}}
+				{{--var randomY = Math.ceil(Math.random() * 800) - 41;--}}
+				{{--var random = Math.ceil(Math.random() * 2);--}}
+				{{--$('.monitor-border').find('.dot-' + random).css({--}}
+					{{--left: randomX,--}}
+					{{--top: randomY--}}
+				{{--})--}}
+			{{--}, 1000 * 30)--}}
+		{{--})--}}
+	{{--</script>--}}
 </body>
 </html>
